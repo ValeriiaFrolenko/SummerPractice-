838989b29797430e378f82be6b1785428a63fba9
ValeriiaFrolenko <v.frolenko@ukma.edu.ua>
2025-06-01 21:07:56 +0300

    реалізація Vector2D

--- CatRobber/src/main/java/utils/Vector2D.java
+++ CatRobber/src/main/java/utils/Vector2D.java
@@ -8,17 +8,22 @@ public class Vector2D {
 
     // Конструктор
     // Ініціалізує вектор з координатами x, y
-    public Vector2D(double x, double y) {}
+    public Vector2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
 
     // Додає інший вектор
     // Отримує other, повертає новий Vector2D
-    public Vector2D add(Vector2D other) { return null; }
+    public Vector2D add(Vector2D other) {
+        return new Vector2D(x + other.x, y + other.y);
+    }
 
     // Обчислює відстань до іншого вектора
     // Отримує other, повертає відстань (число)
-    public double distance(Vector2D other) { return 0.0; }
+    public double distance(Vector2D other) { return Math.sqrt(x * x + y * y) ; }
 
     // Множить вектор на скаляр
     // Отримує scalar, повертає новий Vector2D
-    public Vector2D multiply(double scalar) { return null; }
+    public Vector2D multiply(double scalar) { return new Vector2D(this.x * scalar, this.y * scalar); }
 }
