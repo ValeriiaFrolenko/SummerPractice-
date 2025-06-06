69f6c2fb26de5e7b1ba84e896b6511c85fc086c5
ValeriiaFrolenko <v.frolenko@ukma.edu.ua>
2025-06-06 22:43:19 +0300

    розділила логіку координат зображення і координат колізій

--- CatRobber/src/main/java/interfaces/Positioned.java
+++ CatRobber/src/main/java/interfaces/Positioned.java
@@ -7,10 +7,13 @@ import utils.Vector2D;
 public interface Positioned {
     // Повертає позицію об’єкта, передає в GameManager для колізій
     Vector2D getPosition();
+    Vector2D getImaginePosition();
 
     // Встановлює позицію, отримує від GameManager (наприклад, при русі)
     void setPosition(Vector2D position);
+    void setImaginePosition(Vector2D position);
 
     // Повертає межі для колізій, передає в GameManager
     Bounds getBounds();
+    Bounds getImagineBounds();
 }
